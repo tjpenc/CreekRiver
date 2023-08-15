ae8968b45a161969fffaac78c6fd10684f7be50a
Thomas Pencarinha <tjpencarinha@gmail.com>
2023-08-15 19:53:41 -0400

    Created call to get all reservations from the database with all information associated with the reservation

--- Program.cs
+++ Program.cs
@@ -84,5 +84,15 @@ app.MapPut("/api/campsites/{id}", (CreekRiverDbContext db, int id, Campsite camp
     return Results.NoContent();
 });
 
+app.MapGet("/api/reservations", (CreekRiverDbContext db) =>
+{
+    return db.Reservations
+        .Include(r => r.UserProfile)
+        .Include(r => r.Campsite)
+        .ThenInclude(c => c.CampsiteType)
+        .OrderBy(res => res.CheckinDate)
+        .ToList();
+});
+
 app.Run();
 
